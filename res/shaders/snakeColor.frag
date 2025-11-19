cbe2fb141b766c85fec05dd40790ce9424873cd5
Juan Savedra <juansavedra827@gmail.com>
2025-11-19 13:43:26 -0300

    feat: Generating food

--- res/shaders/snakeColor.frag
+++ res/shaders/snakeColor.frag
@@ -1,7 +1,9 @@
 #version 330 core
+
 out vec4 FragColor;
+uniform vec4 objectColor;
 
 void main()
 {
-  FragColor = vec4(0.26, 0.77, 0.26, 1.0);
+  FragColor = objectColor;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,13 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <cstdlib>
+#include <ctime>
 
 #include "Shader.h"
 #include "Snake.h"
 
-void processInput(GLFWwindow *window, Snake& snake)
+void processInput(GLFWwindow *window, Snake &snake)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
@@ -23,16 +25,54 @@ void processInput(GLFWwindow *window, Snake& snake)
         snake.changeDirection(Direction::LEFT);
 }
 
+int randomRange(int min, int max)
+{
+    return min + (rand() % (max - min + 1));
+}
+
+GridPosition generateNewFood(const Snake &snake, int gridWidth, int gridHeight)
+{
+    GridPosition newPosition;
+    bool validPosition = false;
+
+    while (!validPosition)
+    {
+        newPosition.x = randomRange(0, gridWidth - 1);
+        newPosition.y = randomRange(0, gridHeight - 1);
+
+        validPosition = true;
+
+        for (const auto &segment : snake.getBody())
+        {
+            if (newPosition == segment)
+            {
+                validPosition = false;
+                break;
+            }
+        }
+    }
+
+    return newPosition;
+}
+
+void drawSquare(unsigned int VAO, unsigned int modelLoc, const GridPosition &position,
+                const glm::vec4 &color, unsigned int colorLoc)
+{
+    glUniform4f(colorLoc, color.r, color.g, color.b, color.a);
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::translate(model, glm::vec3((float)position.x + 0.5f,
+                           (float)position.y + 0.5f, 0.0f));
+    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    glBindVertexArray(0);
+}
+
 void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
     glViewport(0, 0, width, height);
 }
-
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
-
-const int GRID_WIDTH = 20;
-const int GRID_HEIGHT = 20;
 
 glm::mat4 createOrthographicProjection(float left, float right, float bottom, float top,
                                        float near, float far)
@@ -42,6 +82,12 @@ glm::mat4 createOrthographicProjection(float left, float right, float bottom, fl
 
 int main()
 {
+    const unsigned int SCR_WIDTH = 800;
+    const unsigned int SCR_HEIGHT = 600;
+    
+    const int GRID_WIDTH = 20;
+    const int GRID_HEIGHT = 20;
+
     if (!glfwInit())
     {
         std::cerr << "Falha ao inicializar GLFW" << std::endl;
@@ -69,6 +115,9 @@ int main()
         return -1;
     }
 
+    srand((unsigned int)time(0));
+    GridPosition food = {15, 10};
+
     float vertices[] = {
         // Triângulo 1
         -0.5f, -0.5f, 0.0f, // Inferior Esquerdo
@@ -82,7 +131,7 @@ int main()
     };
 
     float lastTime = 0.0f;
-    const float MOVE_INTERVAL = 0.2f;
+    const float MOVE_INTERVAL = 0.15f;
 
     Snake snake(5, 5);
 
@@ -104,51 +153,51 @@ int main()
 
     Shader shader("res/shaders/snakeVertex.vert", "res/shaders/snakeColor.frag");
     shader.use();
-    
-    unsigned int projectionLoc = glGetUniformLocation(shader.ID, "projection");
-    float aspectRatio = (float)SCR_WIDTH / (float)SCR_HEIGHT;
 
+    unsigned int projectionLoc = glGetUniformLocation(shader.ID, "projection");
+    unsigned int modelLoc = glGetUniformLocation(shader.ID, "model");
+    unsigned int colorLoc = glGetUniformLocation(shader.ID, "objectColor");
+
+    float aspectRatio = (float)SCR_WIDTH / (float)SCR_HEIGHT;
     glm::mat4 projection = glm::ortho(
         0.0f, (float)GRID_WIDTH * aspectRatio,
         0.0f, (float)GRID_HEIGHT,
         -1.0f, 1.0f);
 
     glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
-    
-    unsigned int modelLoc = glGetUniformLocation(shader.ID, "model");
 
     while (!glfwWindowShouldClose(window))
-    {
+    {   
+        //Input
         processInput(window, snake);
 
+        //Timer
         float currentTime = (float)glfwGetTime();
-        if (currentTime - lastTime >= MOVE_INTERVAL) {
+        if (currentTime - lastTime >= MOVE_INTERVAL)
+        {
             snake.move(false);
-
-            if (snake.isOutOfBounds(GRID_WIDTH, GRID_HEIGHT) || snake.isSelfCollision()) {
+            
+            if (snake.isOutOfBounds(GRID_WIDTH, GRID_HEIGHT) || snake.isSelfCollision())
+            {
                 glfwSetWindowShouldClose(window, true);
             }
 
             lastTime = currentTime;
         }
 
+        //Reset
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-
         shader.use();
 
-        for (const auto& segment : snake.getBody()) {
-            glm::mat4 model = glm::mat4(1.0f);
-            model = glm::translate(model, glm::vec3(
-                (float)segment.x + 0.5f, 
-                (float)segment.y + 0.5f, 
-                0.0f)
-            );
+        //Food render
+        drawSquare(VAO, modelLoc, food, glm::vec4(1.0f, 0.0f, 0.0f, 1.0f), colorLoc);
 
-            glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-
-            glBindVertexArray(VAO);
-            glDrawArrays(GL_TRIANGLES, 0, 6);
+        //Snake render
+        glm::vec4 snakeColor(0.0f, 1.0f, 0.0f, 1.0f);
+        for (const auto &segment : snake.getBody())
+        {
+            drawSquare(VAO, modelLoc, segment, snakeColor, colorLoc);
         }
 
         glfwSwapBuffers(window);
